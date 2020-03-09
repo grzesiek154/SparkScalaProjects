eb97cec3f176750fae959bb35b11592f7f67d7f0
Grzesiek-Office <grzegorz.malarski@amadeus.com>
2020-03-09 17:44:02 +0100

    mark lewis scala

--- MarkLewisSparkScala/src/standardscala/ParallelCollections.scala
+++ MarkLewisSparkScala/src/standardscala/ParallelCollections.scala
@@ -1,9 +1,11 @@
 package standardscala
 
-object ParallelCollections {
+object ParallelCollections extends App{
   
-  val arr = Array(4, 2, 7, 3, 9, 1)
+  val arr = Array(4, 2, 7, 3, 9, 1).par
    // (((((0-4)-2)-7)-3)-9)-1
   
-  println(arr.foldLeft(0)(_+_))
+  println(arr.foldLeft(0)(_-_))
+  println(arr.foldRight(0)(_-_))
+  println(arr.fold(0)(_-_))
 }
